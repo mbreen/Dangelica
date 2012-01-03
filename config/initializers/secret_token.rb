3341105a968b33b289595be859bdbd84e6bda643
Michael Breen <github@machinerebel.com>
2012-01-02 22:48:51 -0500

    Initial Check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dangelica::Application.config.secret_token = 'a36b8ddfbb9e700eae091b2f2431c1e81983352d6b76ab4d6b3afd4788fa00512bd5458008a17c20f9b3c70cf49c0f13070c06a380125515b2a5f7d1c140db5d'
